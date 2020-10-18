86b989465ed5c58d7170ff520b306b4b87426f36
d9dch <66538179+d9dch@users.noreply.github.com>
2020-10-18 00:38:33 -0500

    Single String Input prediction
    ** must be in same directory as model

--- load_model_single_prediction.py
+++ load_model_single_prediction.py
@@ -6,10 +6,10 @@ tf.keras.backend.clear_session()
 def single_predict(raw_string_input):
     loaded_model = tf.keras.models.load_model('text_product_classify')
 
-    test_input = 'XBOX ONE 1TB'
+    test_input = raw_string_input
     preds = loaded_model.predict([test_input])
     print(preds)
     return preds
 
 # EXTRA FUNCTION CALL FOR TESTING, THIS WOULD BE CALLED IN MAIN UI LOOP
-single_predict('Pjjlaskjfdlakfj sdl;kfjdkl')
+single_predict('smartphone apple')
