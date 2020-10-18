f0ee8ceaed316adc4ae449239475b80bfb680d47
d9dch <66538179+d9dch@users.noreply.github.com>
2020-10-17 19:00:15 -0500

    Working multiclass text classifier with dummy data
    Working with dummy data. Need to add model saving for a backend implementation

--- categorical_text_classification.py
+++ categorical_text_classification.py
@@ -9,19 +9,13 @@ if __name__ == "__main__":
     print('Executing Text analysis')
     # similar to a movie review example (good or bad based on text in review)
     # categorize product category based on the title of the product
+    # web scraper gives us two features, product name and product parent breadcrumb
+    # Train 20%, and then the 80% you will be able to determine label from description
 
     # defining some scope variables for later tuning
     num_samples = 20
     training_percent_split = .2
     validation_percent_split = .8
-
-    # MISSING, but here I will split into to training data and  validation data #####
-    # category_labels = ['food', 'electronics', 'personal-care', 'toys', 'home', 'video-games', 'beauty']
-    # dummy data
-    # category_labels = ['food', 'electronics']
-    # # instead of using string operators the pre trained nnlm model will tokenize our sentences
-    # product_names = ['Sony PlayStation 4 1TB Slim Gaming Console',
-    #                  'OREO Mini Chocolate Sandwich Cookies, 12 - 1 oz Snack Packs']
 
     # Assume data will come in via csv or json or some other pandas DF readable format
     # The drawback is that the data has to be in the same file
@@ -60,23 +54,22 @@ if __name__ == "__main__":
         )
     )
     # modifying the TF docs example on movie reviews
-    # Creating a layer that will tokenize and create the right output shape 'embedding'
+    # Creating a layer that will tokenize and create the right output shape 'embedding' from text
     # don't know if this is capturing the whole string
     # using nnlm dim 50/2 but we can try other models
     embedding = "https://tfhub.dev/google/nnlm-en-dim50/2"
-
-
     hub_layer = hub.KerasLayer(embedding, input_shape=[], dtype=tf.string, trainable=True)
+
     # model layer definition (try with different activations although relu is usually best)
     model = tf.keras.Sequential()
     model.add(hub_layer)
     model.add(tf.keras.layers.Dense(16, activation='relu'))
-    model.add(tf.keras.layers.Dense(1))
+    model.add(tf.keras.layers.Dense(2))
     model.summary()
 
     print('Are we compiling? ')
     # model compile, we need to use binary crossentropy for this with more categories
-    model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
+    model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
 
 
